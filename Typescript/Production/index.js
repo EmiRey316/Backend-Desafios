8e1b60101e8e95a4afa7dcc283da7487e6e023d1
Emiliano Rey <emirey316@gmail.com>
2022-03-01 12:43:03 -0300

    Clase2 TypeScript corregido

--- Typescript/Production/index.js
+++ Typescript/Production/index.js
@@ -8,17 +8,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+//Función que transforma el tipo de operación ingresado en el formato correcto. 
+const transformOperationType = (operationType) => {
+    return (operationType[0].toUpperCase() + operationType.slice(1).toLowerCase());
+};
 const operation = (num1, num2, operationType) => __awaiter(void 0, void 0, void 0, function* () {
-    let importedOperation = yield import(`./OperationsClasses/${operationType}.js`);
+    //Tranformo el parámetro operationType ingresado en el formato correcto.
+    let operationTypeTransformed = transformOperationType(operationType);
+    //Verifico si el tipo de operación ingresado se corresponde con uno de los habilitados.
+    const possibleOperations = ["Sum", "Substraction", "Multiply", "Divide"];
+    if (!possibleOperations.includes(operationTypeTransformed)) {
+        return "La operación solicitada no es válida";
+    }
+    //Importación dinámica del módulo requerido.
+    let importedOperation = yield import(`./OperationsClasses/${operationTypeTransformed}.js`);
     return new Promise((resolve) => {
-        const newOperation = new importedOperation.default(num1, num2);
+        let newOperation = new importedOperation.default(num1, num2);
         resolve(newOperation.result(num1, num2));
     });
 });
 const operations = () => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(`Suma: ${yield operation(1, 2, "Sum")}`);
+    console.log(yield operation(1, 2, "invalid"));
+    console.log(`Suma: ${yield operation(1, 2, "sum")}`);
     console.log(`Resta: ${yield operation(10, 5, "Substraction")}`);
-    console.log(`Multiplicación: ${yield operation(10, 10, "Multiply")}`);
+    console.log(`Multiplicación: ${yield operation(10, 10, "multiply")}`);
     console.log(`División: ${yield operation(6, 2, "Divide")}`);
 });
 operations();
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,17 +1,34 @@
+//Función que transforma el tipo de operación ingresado en el formato correcto. 
+const transformOperationType = (operationType: string) => {
+    return (operationType[0].toUpperCase() + operationType.slice(1).toLowerCase());
+}
+
+
 const operation = async (num1: number, num2: number, operationType: string) => {
-    let importedOperation = await import(`./OperationsClasses/${operationType}.js`);
+    //Tranformo el parámetro operationType ingresado en el formato correcto.
+    let operationTypeTransformed = transformOperationType(operationType);
+
+    //Verifico si el tipo de operación ingresado se corresponde con uno de los habilitados.
+    const possibleOperations:Array<string> = ["Sum", "Substraction", "Multiply", "Divide"];
+    if(!possibleOperations.includes(operationTypeTransformed)) {
+        return "La operación solicitada no es válida";
+    }
+    
+    //Importación dinámica del módulo requerido.
+    let importedOperation = await import(`./OperationsClasses/${operationTypeTransformed}.js`);
     
     return new Promise((resolve) => {
-        const newOperation = new importedOperation.default(num1, num2);
+        let newOperation = new importedOperation.default(num1, num2);
         resolve(newOperation.result(num1, num2));
     })
 }
 
 
 const operations = async () => {
-    console.log(`Suma: ${await operation(1, 2, "Sum")}`);
+    console.log(await operation(1, 2, "invalid"))
+    console.log(`Suma: ${await operation(1, 2, "sum")}`);
     console.log(`Resta: ${await operation(10, 5, "Substraction")}`);
-    console.log(`Multiplicación: ${await operation(10, 10, "Multiply")}`);
+    console.log(`Multiplicación: ${await operation(10, 10, "multiply")}`);
     console.log(`División: ${await operation(6, 2, "Divide")}`);
 }
 
